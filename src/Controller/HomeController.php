177190886f11d018f4ce21e51ad875f553192581
Lilian <lilian.dorazio@hotmail.fr>
2021-07-08 10:50:48 +0200

    Test de rendu Champion + APIKEY .env

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\API\LOL\ApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var ApiClient
+     */
+    private $apiClient;
+
+    /**
+     * HomeController constructor.
+     */
+    public function __construct()
+    {
+        $this->apiClient = new ApiClient($_ENV['APIKEY']);
+    }
+
+
+    /**
      * @Route("/home", name="home")
      */
     public function index(): Response
     {
+        $champions = $this->apiClient->championApi()->GetAllChampion();
+        dd($champions);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
