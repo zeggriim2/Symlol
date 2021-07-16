f638f4840f33ad36b70573095c160dfc5745d370
Lilian <lilian.dorazio@hotmail.fr>
2021-07-16 11:42:09 +0200

    Logging Symfony

--- src/Service/API/LOL/BaseApi.php
+++ src/Service/API/LOL/BaseApi.php
@@ -3,7 +3,9 @@
 
 namespace App\Service\API\LOL;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
@@ -12,6 +14,9 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
  */
 class BaseApi
 {
+    const CODE_HTTP_INFO    = [100, 101, 102, 103];
+    const CODE_HTTP_SUCCESS  = [200,201,202];
+
     /**
      * @var ApiClient
      */
@@ -30,12 +35,17 @@ class BaseApi
      * @var mixed
      */
     protected $apiKey;
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
 
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct(HttpClientInterface $httpClient, LoggerInterface $logger)
     {
-        $this->httpClient = $httpClient;
-        $this->lang = "fr_FR";
-        $this->apiKey = $_ENV['APIKEY'];
+        $this->httpClient   = $httpClient;
+        $this->logger       = $logger;
+        $this->lang         = "fr_FR";
+        $this->apiKey       = $_ENV['APIKEY'];
     }
 
     protected function getLastVersion(): string
@@ -46,9 +56,22 @@ class BaseApi
 
     protected function callApi($url, $method = "GET", $options = []): array
     {
-        $response = $this->httpClient->request($method, $url, $options);
-        if ($response->getStatusCode() === 200){
-            return $response->toArray();
+        try{
+            $response = $this->httpClient->request($method, $url, $options);
+            $codeHttp = $response->getStatusCode();
+            if (in_array($codeHttp,self::CODE_HTTP_SUCCESS)){
+                $this->logger->info("API SUCCESS", [
+                    'code' => $codeHttp,
+                    'url' => $url
+                ]);
+                return $response->toArray();
+            }elseif (in_array($codeHttp,self::CODE_HTTP_INFO)){
+                $this->logger->info("API INFO", [
+                    'code' => $codeHttp,
+                    'url' => $url
+                ]);
+            }
+        }catch (TransportExceptionInterface $e) {
         }
     }
 
