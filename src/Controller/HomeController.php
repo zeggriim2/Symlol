fec8df3679a8578e1ded807f2ad5ee417594e7e9
Lilian <lilian.dorazio@hotmail.fr>
2021-07-20 09:41:31 +0200

    Clean

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,21 @@
 
 namespace App\Controller;
 
-use App\Service\API\LOL\ApiClient;
 use App\Service\API\LOL\ChampionApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      * @param ChampionApi $championApi
      * @return Response
      */
     public function index(ChampionApi $championApi): Response
     {
-        $champions = $championApi->GetAllChampion()['data'];
-        return $this->render('home/index.html.twig', [
-            'champions' => $champions,
-        ]);
+//        return $this->redirectToRoute('champions_index');
+        return $this->render('home/index.html.twig');
     }
 }
---
+++ tests/ChampionControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use App\Service\API\LOL\ChampionApi;
+use PHPUnit\Framework\TestCase;
+
+class ChampionControllerTest extends TestCase
+{
+
+
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
