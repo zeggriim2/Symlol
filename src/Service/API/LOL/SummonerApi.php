0370dd66be293ab35a12eced0f4b341603c120fe
Zeggiim <lilian.dorazio@hotmail.fr>
2021-10-26 08:23:54 +0200

    Ajout de tout les appels Summoner By

--- src/Service/API/LOL/SummonerApi.php
+++ src/Service/API/LOL/SummonerApi.php
@@ -7,6 +7,9 @@ class SummonerApi
 
     private const URL_NAME = "https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}";
     private const URL_ACCOUNT = "https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/by-account/{encryptedAccountId}";
+    private const URL_PUUID = "https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{encryptedPUUID}";
+    private const URL_SUMMONER_ID = "https://{platform}.api.riotgames.com/lol/summoner/v4/summoners/{encryptedSummonerId}";
+
 
     public const PLATFORM = [
         'EUW1' => "EUW1", //Europe West
@@ -39,15 +42,17 @@ class SummonerApi
      * @param string $platform
      * @param string $name
      * @return array<mixed>|null
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
     public function getSummonerBySummonerName(string $platform, string $name)
     {
-        $url = $this->baseApi->constructUrl(self::URL_NAME, ['platform' => $platform, 'name' => $name]);
+        $url = $this->baseApi->constructUrl(
+            self::URL_NAME,
+            [
+                'platform' => $platform,
+                'name' => $name
+            ]
+        );
+
         return $this->baseApi->callApi($url, "GET", [
             'headers' => [
                 'X-Riot-Token' => $this->baseApi->apiKey,
@@ -55,8 +60,61 @@ class SummonerApi
         ]);
     }
 
-    public function getSummonerByAccountID()
+    /**
+     * Get Summoner by account ID
+     *
+     * @param string $platform
+     * @param string $accountId
+     * @return array|null
+     */
+    public function getSummonerByAccountID(string $platform,string $accountId)
     {
+        $url = $this->baseApi->constructUrl(
+            self::URL_ACCOUNT,
+            [
+                'platform' => $platform,
+                'encryptedAccountId' => $accountId
+            ]
+        );
 
+        return $this->baseApi->callApi($url);
+    }
+
+    /**
+     * Get Summoner by PUUID
+     * @param string $platform
+     * @param string $puuid
+     * @return array|null
+     */
+    public function getSummonerByPuuid(string $platform,string $puuid)
+    {
+        $url = $this->baseApi->constructUrl(
+            self::URL_PUUID,
+            [
+                'platform' => $platform,
+                'encryptedPUUID' => $puuid
+            ]
+        );
+
+        return $this->baseApi->callApi($url);
+    }
+
+    /**
+     * Get Summoner by Summoner ID
+     * @param string $platform
+     * @param string $summonerId
+     * @return array|null
+     */
+    public function getSummonerBySummonerId(string $platform,string $summonerId)
+    {
+        $url = $this->baseApi->constructUrl(
+            self::URL_SUMMONER_ID,
+            [
+                'platform' => $platform,
+                'encryptedSummonerId' => $summonerId
+            ]
+        );
+
+        return $this->baseApi->callApi($url);
     }
 }
