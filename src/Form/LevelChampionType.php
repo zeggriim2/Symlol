f09cea2ccd33fb983e29f47800c9465ae503d8f8
Lilian <lilian.dorazio@hotmail.fr>
2021-11-12 11:21:24 +0100

    Optimisation + clean LevelChampion

--- src/Form/LevelChampionType.php
+++ src/Form/LevelChampionType.php
@@ -4,21 +4,24 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LevelChampionType extends AbstractType
 {
+    /** CONSTANTE **/    
+    private const LEVEL_MIN = 1;
+    private const LEVEL_MAX = 18;
+    /** CONSTANTE **/
     /**
      * @var RequestStack
      */
     private $requestStack;
 
+    
     /**
-     * RankType constructor.
+     * LevelChampionType constructor.
      */
     public function __construct(RequestStack $requestStack)
     {
@@ -29,30 +32,15 @@ class LevelChampionType extends AbstractType
     {
         $builder
             ->add('level', ChoiceType::class, [
-                    'choices'  => [
-                        '1' => 1,
-                        '2' => 2,
-                        '3' => 3,
-                        '4' => 4,
-                        '5' => 5,
-                        '6' => 6,
-                        '7' => 7,
-                        '8' => 8,
-                        '9' => 9,
-                        '10' => 10,
-                        '11' => 11,
-                        '12' => 12,
-                        '13' => 13,
-                        '14' => 14,
-                        '15' => 15,
-                        '16' => 16,
-                        '17' => 17,
-                        '18' => 18
-                    ],
-                    "data" => $this->requestStack->getSession()->get('level')
+                    'choices'  => $this->generateArrayLevel(),
+                    "data" => $this->requestStack->getSession()->get('StatChampionlevel')
             ])
-            ->add('Valider', SubmitType::class)
         ;
+    }
+
+    private function generateArrayLevel(): array 
+    { 
+      return array_combine(range(self::LEVEL_MIN,self::LEVEL_MAX), range(self::LEVEL_MIN,self::LEVEL_MAX));
     }
 
     public function configureOptions(OptionsResolver $resolver)
